7bd99412ce58a7ee5d40b7bd89ede5613042a98f
yoonsun lee <yoonsunlee150@gmail.com>
2022-05-27 09:05:35 -0400

    three-svelte-scaffold

---
+++ 00svelte3d/cubed/src/routes/index.svelte
@@ -0,0 +1,13 @@
+
+<script>
+	import * as THREE from 'three';
+	import * as SC from 'svelte-cubed';
+</script>
+
+<SC.Canvas antialias background={new THREE.Color('lt-grey')}>
+	<SC.Mesh 
+    material={new THREE.MeshStandardMatrial }
+    geometry={new THREE.BoxGeometry()} />
+	<SC.PerspectiveCamera position={[1, 1, 3]} />
+    <SC.OrbitControls />
+</SC.Canvas>
