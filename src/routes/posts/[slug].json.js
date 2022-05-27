f6467c56273d37301a3c037d79263b64158f6046
yoonsun lee <yoonsunlee150@gmail.com>
2022-05-26 23:54:47 -0400

    page update

--- src/routes/posts/[slug].json.js
+++ src/routes/posts/[slug].json.js
@@ -5,31 +5,31 @@ export const get = async (req) => {
     const slug = req.params.slug
     try{
         const query = gql`
-          query Post($slug:String!){
-            post(where:{slug:$slug}){
-                title
-                date
-                tags
-                author{
-                name
-                authorTitle: title
-                picture {
-                    url(transformation:{image:{resize:{fit:clip, height:50, width:50}}})
+         query Post($slug:String!){
+                post(where:{slug:$slug}){
+                    title
+                    date
+                    tags
+                    author{
+                    name
+                    authorTitle: title
+                    picture {
+                        url(transformation:{image:{resize:{fit:clip, height:50, width:50}}})
+                    }
+                    }
+                    content{
+                    html
+                    }
+                    coverImage{
+                    url
+                    }
                 }
                 }
-                content{
-                html
-                }
-                coverImage{
-                url
-                }
-            }
-            }
         `
 
 
 const variables = {slug}
-const {post} = await client.request {query, variables}
+const {post} = await client.request{query}
 
 return {
             status:200,
